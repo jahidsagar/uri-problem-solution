639dcaf0f3eb6c8d4f110c72d80965de6a5e891e
jahidsagar <jahidsagar@gmail.com>
2016-03-24 19:56:34 +0600

    Above the Secundary Diagonal

---
+++ 1185.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    double l,sum =0.0;
+    char m;
+    std::cin>>m;
+    double k[12][12];
+    for(i=0;i<12;i++)
+    {
+        for(j=0;j<12;j++)
+        {
+           std::cin>>k[i][j];
+           if(j < 11-i)
+            sum += k[i][j];
+        }
+    }
+    if(m == 'S')
+            printf("%.1lf\n",sum);
+    else
+        printf("%.1lf\n",sum/66.00);
+
+        return 0;
+}
