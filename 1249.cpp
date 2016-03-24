35254ee11090a807c82406fbba85af160aeffb76
jahidsagar <jahidsagar@gmail.com>
2016-03-24 19:36:03 +0600

    Rot13
    https://www.urionlinejudge.com.br/judge/en/problems/view/1249/solution

---
+++ 1249.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <set>
+#include <stdio.h>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    while(scanf("%c",&c) != EOF)
+    {
+        if(c >='a' && c <='z')
+        {
+            if(c+13 >'z') printf("%c",c-13);
+            else printf("%c",c+13);
+        }
+        else if(c >='A' && c <='Z')
+        {
+            if(c+13 > 'Z') printf("%c",c-13);
+            else printf("%c",c+13);
+        }
+        else printf("%c",c);
+    }
+    return 0;
+}
