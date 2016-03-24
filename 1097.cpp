bd0422115a2fd13f1c9071c56abdba69606c5c12
jahidsagar <jahidsagar@gmail.com>
2016-03-24 19:42:24 +0600

    Sequence IJ 3

---
+++ 1097.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int i , j , a , b,m;
+   a = 1; b = 7; m = 0;
+   while(1)
+   {
+       ++m;
+       printf("I=%d J=%d\n",a,b--);
+       if(m == 3 && a == 9) break;
+       else if(m == 3)
+       {
+           a = a+2; b = b+5;
+           m = 0;
+       }
+
+   }
+   return 0;
+}
